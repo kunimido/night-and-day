f0aee681baf46696849444046292a1a409e5c3c6
kunimido <kunimido@users.noreply.github.com>
2019-05-09 10:23:33 +0200

    Initial Commit

---
+++ Night and Day/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Night and Day
+//
+//  Created by Dominique on 09/05/2019.
+//  Copyright © 2019 Dominique. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

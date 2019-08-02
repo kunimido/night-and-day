fbf29999315319cc4f4a1738235a351297ced4de
kunimido <kunimido@users.noreply.github.com>
2019-08-02 21:59:25 +0200

    Migrate to zsh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/zsh
 
-SRC="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+SRC="$(cd $(dirname "$0") && pwd 2>/dev/null)"
 PRODUCT=night-and-day
 SERVICE=com.github.kunimido.${PRODUCT}
 PLIST=${SERVICE}.plist
 
 cd "$SRC"
-xcodebuild install DSTROOT="$HOME" INSTALL_PATH=bin PRODUCT_NAME="$PRODUCT"
+xcodebuild install DSTROOT="$HOME" INSTALL_PATH="bin" PRODUCT_NAME="$PRODUCT"
 xcodebuild clean
 
 cd "$HOME/Library/LaunchAgents/"
